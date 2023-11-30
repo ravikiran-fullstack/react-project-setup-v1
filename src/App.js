1236e0fe905129c7179e7e902712779ae1114f83
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-11-29 19:31:36 -0500

    initial setup with debounce function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import './index.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const debounce = (func, delay) => {
+    let debounceTimer;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+
+  // write code to utilize debounce function
+  const debounceFunc = debounce((value) => {
+    console.log(value);
+    setSearchTerm(value);
+  }, 100);
+
+  const handleInputChange = (event) => {
+    debounceFunc(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <p>Search functionality with debounce function</p>
+        <input type="text" onChange={handleInputChange} value={searchTerm} />
       </header>
     </div>
   );
