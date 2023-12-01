27867aa6ff580cab635a3dc3d185bbfbcb000f9a
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-12-01 00:41:12 -0500

    updated debounce time to 150 ms

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App () {
   const debounceFunc = debounce((value) => {
     console.log(value);
     setSearchTerm(value);
-  }, 100);
+  }, 150);
 
   const handleInputChange = (event) => {
     debounceFunc(event.target.value);
